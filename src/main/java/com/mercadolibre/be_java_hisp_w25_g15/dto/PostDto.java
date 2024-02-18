929cbcc5b3466eff0c056f16bb018d150d2292a2
Martin Sergio Perren <martin.perren@mercadolibre.com>
2024-02-18 15:42:40 -0300

    US-0005 WIP

--- src/main/java/com/mercadolibre/be_java_hisp_w25_g15/dto/PostDto.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w25_g15/dto/PostDto.java
@@ -1,10 +1,13 @@
 package com.mercadolibre.be_java_hisp_w25_g15.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDate;
 
 public record PostDto (
-        int userId,
-        LocalDate date,
+        int user_id,
+        @JsonFormat(pattern = "dd-MM-yyyy") LocalDate date,
         ProductDto product,
         int category,
         double price
--- src/main/java/com/mercadolibre/be_java_hisp_w25_g15/model/Post.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w25_g15/model/Post.java
@@ -1,5 +1,7 @@
 package com.mercadolibre.be_java_hisp_w25_g15.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,15 +16,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Post {
     static final AtomicInteger idGenerator = new AtomicInteger();
+    @JsonIgnore
     int id;
-    User user;
+    @JsonProperty("user_id")
+    int userId;
     LocalDate date;
     Product product;
     int category;
     double price;
-    public Post(User user, LocalDate date, Product product, int category, double price) {
+    public Post(int user, LocalDate date, Product product, int category, double price) {
         this.id = idGenerator.incrementAndGet();
-        this.user = user;
+        this.userId = user;
         this.date = date;
         this.product = product;
         this.category = category;
