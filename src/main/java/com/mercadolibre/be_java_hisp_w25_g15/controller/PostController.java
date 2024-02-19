c85fd0a3bf2f57aee273cb99a5f2fe5183879936
Martin Sergio Perren <martin.perren@mercadolibre.com>
2024-02-19 09:38:03 -0300

    US-0005 Validaciones agregadas

--- src/main/java/com/mercadolibre/be_java_hisp_w25_g15/controller/PostController.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w25_g15/controller/PostController.java
@@ -3,22 +3,24 @@ package com.mercadolibre.be_java_hisp_w25_g15.controller;
 import com.mercadolibre.be_java_hisp_w25_g15.dto.PostDto;
 import com.mercadolibre.be_java_hisp_w25_g15.dto.response.PostGetListDto;
 import com.mercadolibre.be_java_hisp_w25_g15.service.IPostService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+
 @RequestMapping("/products")
 public class PostController {
     private final IPostService iPostService;
 
 
     @PostMapping("/post")
-    ResponseEntity<PostDto> createPost(@RequestBody PostDto post){
+    ResponseEntity<PostDto> createPost(@RequestBody @Valid PostDto post){
         return new ResponseEntity<>(iPostService.createPost(post), HttpStatus.OK);
     }
     @GetMapping("/followed/{userId}/list")
--- src/main/java/com/mercadolibre/be_java_hisp_w25_g15/dto/ProductDto.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w25_g15/dto/ProductDto.java
@@ -1,11 +1,14 @@
 package com.mercadolibre.be_java_hisp_w25_g15.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record ProductDto(
-        int product_id,
-        String product_name,
-        String type,
-        String brand,
-        String color,
-        String notes
+        @Positive(message = "El product_id debe ser un número entero positivo") @NotNull(message = "El product_id no puede ser nulo") int product_id,
+        @NotNull(message = "El product_name no puede ser nulo") String product_name,
+        @NotNull(message = "El type no puede ser nulo") String type,
+        @NotNull(message = "La marca no puede ser nula") String brand,
+        @NotNull(message = "El color no puede ser nulo") String color,
+        @NotNull(message = "Las notas no pueden ser nulas") String notes
 ) {
 }
