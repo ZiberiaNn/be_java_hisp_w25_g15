62a162437a1e7a1db51c1a64330373f0c214d11c
Santiago Jaramillo <santiago.jaramilloocampo@mercadolibre.com.co>
2024-02-19 11:26:49 -0500

    feature: endpoint to get posts by seller ID in last two weeks

--- src/main/java/com/mercadolibre/be_java_hisp_w25_g15/service/impl/PostService.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w25_g15/service/impl/PostService.java
@@ -2,8 +2,6 @@ package com.mercadolibre.be_java_hisp_w25_g15.service.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.mercadolibre.be_java_hisp_w25_g15.dto.PostDto;
 import com.mercadolibre.be_java_hisp_w25_g15.dto.response.PostGetListDto;
 import com.mercadolibre.be_java_hisp_w25_g15.exception.ConflictException;
@@ -17,14 +15,14 @@ import com.mercadolibre.be_java_hisp_w25_g15.service.IPostService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class PostService implements IPostService {
-    private IPostRepository postRepository;
-    private IUserRepository userRepository;
+    private final IPostRepository postRepository;
+    private final IUserRepository userRepository;
 
     public PostService(IPostRepository postRepository, IUserRepository userRepository) {
         this.postRepository = postRepository;
@@ -41,14 +39,14 @@ public class PostService implements IPostService {
         Post post = objectMapper.convertValue(postDto, Post.class);
         Optional<User> user = userRepository.getUserById(postDto.user_id());
         if (user.isPresent()) {
-           //si el usuario es Seller puedo publicar
+           // si el usuario es Seller puedo publicar
             if((user.get() instanceof Seller)){
                 post.setUserId(user.get().getId());
             }else {
                 throw new ConflictException("User must be a seller to create a post");
             }
             post.setUserId(user.get().getId());
-        }else{
+        } else {
             throw new NotFoundException("User not found");
         }
 
@@ -59,9 +57,30 @@ public class PostService implements IPostService {
     }
 
     @Override
-    public PostGetListDto getPostsBySellerIdLastTwoWeeks(int sellerId) {
-        return null;
+    public PostGetListDto getPostsBySellerIdLastTwoWeeks(int userId) {
+        //TODO : Unificar mappers en utils
+        ObjectMapper objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        objectMapper.registerModule(javaTimeModule);
+
+        User user = userRepository.getUserById(userId)
+                .orElseThrow(() -> new NotFoundException("User with id " +userId+ "not found."));
+        if(user.getFollowed() == null || user.getFollowed().isEmpty()) {
+            throw new NotFoundException("The user with id "+userId+" has no followed users");
+        }
+        List<PostDto> postDtoList =  new ArrayList<>();
+        user.getFollowed().forEach(followedUser -> postDtoList.addAll(
+                this.postRepository.findAllPostsBySellerIdBetweenDateRange(
+                        followedUser.getId(), LocalDate.now().minusWeeks(2), LocalDate.now()
+                        ).stream().map(p -> objectMapper.convertValue(p, PostDto.class)).toList()
+                )
+        );
+        if(postDtoList.isEmpty()){
+            throw new NotFoundException("The users followed by the user with id " +userId+ " haven´t posted in the last two weeks.");
+        }
+        return new PostGetListDto(
+                userId,
+                postDtoList
+        );
     }
-
-
 }
