c58de805e58026b2ca3f08eb1e4850e9d92da33e
svillegasbed_meli <samuel.villegasbedota@mercadolibre.com.co>
2024-02-19 10:26:18 -0500

    Service to count quantity of seller followers is done

--- src/main/java/com/mercadolibre/be_java_hisp_w25_g15/controller/UserController.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w25_g15/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mercadolibre.be_java_hisp_w25_g15.controller;
 
 import com.mercadolibre.be_java_hisp_w25_g15.dto.request.UnfollowDto;
+import com.mercadolibre.be_java_hisp_w25_g15.dto.response.CountFollowersDto;
 import com.mercadolibre.be_java_hisp_w25_g15.dto.response.MessageResponseDto;
 import com.mercadolibre.be_java_hisp_w25_g15.dto.response.UserDto;
 import com.mercadolibre.be_java_hisp_w25_g15.service.IUserService;
@@ -23,26 +24,36 @@ public class UserController {
     public ResponseEntity<MessageResponseDto> followUser(
             @PathVariable int userId,
             @PathVariable int userIdToFollow
-    ){
+    ) {
         return new ResponseEntity<>(
                 this.userService.followSeller(userId, userIdToFollow),
                 HttpStatus.OK
         );
     }
 
+    @GetMapping("/{userId}/followers/count")
+    public ResponseEntity<CountFollowersDto> countFollowers(
+            @PathVariable int userId
+    ) {
+        return new ResponseEntity<>(
+                this.userService.countFollowersByUserId(userId),
+                HttpStatus.OK
+        );
+    }
+
 
     @PostMapping("/{userId}/unfollow/{userIdToUnfollow}")
-    public ResponseEntity<MessageResponseDto> unfollowUser(@RequestBody UnfollowDto unfollowDto){
-        return  new ResponseEntity<>(null, HttpStatus.OK);
+    public ResponseEntity<MessageResponseDto> unfollowUser(@RequestBody UnfollowDto unfollowDto) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
     @GetMapping("/get-users")
-    public ResponseEntity<List<UserDto>> getAllUsers(){
+    public ResponseEntity<List<UserDto>> getAllUsers() {
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/users/{userId}/followed/list")
-    public ResponseEntity<UserDto> getAllFollowedByUser(@PathVariable int userId){
+    public ResponseEntity<UserDto> getAllFollowedByUser(@PathVariable int userId) {
         return new ResponseEntity<>(userService.findAllFollwedByUser(userId), HttpStatus.OK);
     }
 }
