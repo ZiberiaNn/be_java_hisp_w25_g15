b0862592effada4365b9f4595787f7b58225b4ce
Santiago Jaramillo <santiago.jaramilloocampo@mercadolibre.com.co>
2024-02-21 10:36:42 -0500

    fix Post auto increment ID

--- src/main/java/com/mercadolibre/be_java_hisp_w25_g15/model/Post.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w25_g15/model/Post.java
@@ -1,10 +1,8 @@
 package com.mercadolibre.be_java_hisp_w25_g15.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -33,5 +31,8 @@ public class Post {
         this.product = product;
         this.category = category;
         this.price = price;
+    }
+    public void setPostIdAuto(){
+        this.id = idGenerator.incrementAndGet();
     }
 }
